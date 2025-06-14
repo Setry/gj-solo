89200fad150a217ff756f68bb5d150012d8f944a
MrLetsplay <mr.letsplay2003@gmail.com>
2025-06-14 20:31:12 +0200

    Add some player logic

--- scripts/CharSprite.gd
+++ scripts/CharSprite.gd
@@ -1,6 +1,22 @@
 extends Sprite2D
 
+var hue: float = 0
+var pressed: bool = false
+var velocity: Vector2 = Vector2()
 
 func _process(delta):
-	sprite.modulate = Color(0, 0, 1)
-	sprite.modulate = Color(1, 1, 1)
+	if pressed:
+		hue += 0.005
+	self.modulate = Color.from_hsv(hue, 1, 1, 1)
+	self.position += velocity
+	#self.modulate = Color(0, 0, 1)
+	#self.modulate = Color(1, 1, 1)
+	
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("press"):
+		velocity = Vector2(randf() - 0.5, randf() - 0.5).normalized()
+		print("test")
+		pressed = true
+	elif event.is_action_released("press"):
+		pressed = false
