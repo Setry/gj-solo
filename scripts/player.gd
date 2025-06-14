f92df928c5bcd42b88ac2bf1cc871eeba414536f
Setry <kurousagi@gmx.de>
2025-06-14 21:34:48 +0200

    added vars

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,14 @@
 extends CharacterBody2D
 
+var pos : Vector2;
+var old_pos : Vector2;
+var moving : bool;
 var rotation_speed = 1.5 
 
-func _process(delta):
+func _ready():
+	old_pos = global_position;
+	pos = global_position;
+
+func _physics_process(delta):
 	self.rotate(rotation_speed)
+	
