256d2498c39e13ee92c412c6ed120c263c3d9fac
MrLetsplay <mr.letsplay2003@gmail.com>
2025-06-14 20:49:41 +0200

    Rotate player, Move on press

--- scripts/CharSprite.gd
+++ scripts/CharSprite.gd
@@ -1,22 +1,25 @@
 extends Sprite2D
 
+const MOVEMENT_SPEED: float = 5
+const ROTATION_SPEED: float = 0.1
+
+var indicator: Sprite2D
 var hue: float = 0
-var pressed: bool = false
 var velocity: Vector2 = Vector2()
+var angle: float = 0
+
+func _ready() -> void:
+	self.indicator = get_parent().get_node("Indicator") as Sprite2D
 
 func _process(delta):
-	if pressed:
-		hue += 0.005
+	hue += 0.005
 	self.modulate = Color.from_hsv(hue, 1, 1, 1)
 	self.position += velocity
-	#self.modulate = Color(0, 0, 1)
-	#self.modulate = Color(1, 1, 1)
-	
-	
+	self.angle += 0.1
+	self.angle = fmod(self.angle, 6.28)
+	self.indicator.position = self.position + Vector2(cos(angle), sin(angle)).normalized() * 20
+	self.indicator.rotation = angle
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("press"):
-		velocity = Vector2(randf() - 0.5, randf() - 0.5).normalized()
-		print("test")
-		pressed = true
-	elif event.is_action_released("press"):
-		pressed = false
+		velocity = Vector2(cos(angle), sin(angle)).normalized() * MOVEMENT_SPEED
