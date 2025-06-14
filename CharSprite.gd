0f86e0970c31d0ebf7067b4c06b02f987d6e68ec
Setry <kurousagi@gmx.de>
2025-06-14 19:11:19 +0200

    added aread 2d

--- CharSprite.gd
+++ CharSprite.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+
 func _process(delta):
-	$sprite.modulate = Color(0, 0, 1)
-	$sprite.modulate = Color(1, 1, 1)
+	sprite.modulate = Color(0, 0, 1)
+	sprite.modulate = Color(1, 1, 1)
