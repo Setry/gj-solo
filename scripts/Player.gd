7c0512c56ae5db196977e2497f8c4e85c93d4fcd
Setry <kurousagi@gmx.de>
2025-06-16 22:34:07 +0200

    added particles pressing button as short burst indicator

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -50,6 +50,7 @@ func _physics_process(delta):
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("press"):
 		var main = get_tree().root.get_node("Node2D")
+		$CPUParticles2D.emitting = true
 		main.jumped()
 		
 		if attached_to != null:
